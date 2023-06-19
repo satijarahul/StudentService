6e3f401c89a149f6a404f75bdff50ba3aefc0d3c
Rahul Satija <rahul.satija@nagarro.com>
2023-06-19 07:17:37 +0530

    final work

--- src/main/java/com/studentservice/modal/StudentEntity.java
+++ src/main/java/com/studentservice/modal/StudentEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class StudentEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "FirstName")
     private String firstName;
